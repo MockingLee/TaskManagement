3ad8d6ea04dd93cc41b85b33d1a506a75925fb91
DabeyHolmes <793360690@qq.com>
2019-05-13 09:11:43 +0800

    实体
    实体对象

--- src/main/java/com/Picasso/entity/Administrator.java
+++ src/main/java/com/Picasso/entity/Administrator.java
@@ -1,5 +1,27 @@
 package com.Picasso.entity;
 
 public class Administrator {
+    private String user_id;
+    private String passwd;
 
+    public Administrator(String user_id, String passwd) {
+        this.user_id = user_id;
+        this.passwd = passwd;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
 }
--- src/main/java/com/Picasso/entity/Task.java
+++ src/main/java/com/Picasso/entity/Task.java
@@ -6,6 +6,70 @@ package com.Picasso.entity;
  *	Task 实体  
  */
 
-public class Task {
+import java.util.Date;
 
+public class Task {
+    private int task_id;
+    private String title;
+    private String content;
+    private Date init_time;
+    private Date update_time;
+    private int process;
+
+    public Task(int task_id, String title, String content, Date init_time, Date update_time, int process) {
+        this.task_id = task_id;
+        this.title = title;
+        this.content = content;
+        this.init_time = init_time;
+        this.update_time = update_time;
+        this.process = process;
+    }
+
+    public int getTask_id() {
+        return task_id;
+    }
+
+    public void setTask_id(int task_id) {
+        this.task_id = task_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getInit_time() {
+        return init_time;
+    }
+
+    public void setInit_time(Date init_time) {
+        this.init_time = init_time;
+    }
+
+    public Date getUpdate_time() {
+        return update_time;
+    }
+
+    public void setUpdate_time(Date update_time) {
+        this.update_time = update_time;
+    }
+
+    public int getProcess() {
+        return process;
+    }
+
+    public void setProcess(int process) {
+        this.process = process;
+    }
 }
